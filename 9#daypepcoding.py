cebd72f5705ce3806fa123794855bd3f3bba03d0
Saurabh kumar <43346025+skwebdeveloper@users.noreply.github.com>
2020-12-15 22:33:31 +0530

    Day 9 not so bad. Try out Flexbox

--- 9#daypepcoding.py
+++ 9#daypepcoding.py
@@ -1,1 +1,69 @@
-n = int(input())while(n!=0)
+<html>
+<head></head>
+<body>
+<style>
+.flex-container, .main__ul{
+  display: flex;
+  list-style: none;
+  flex-grow: row wrap;
+}
+.flex-item, .inside__ul{
+  width: 100%;
+  display: flex;
+  height: 100px;
+  font-size: 2rem;
+  text-align:center;
+  align-items: center;
+  justify-content: center;
+}
+.inside__ul{
+  background-color:black;
+  color:white;
+}
+
+.flex_item_left{
+  background-color: yellow;
+}
+.flex_item_right{
+  background-color: pink;
+}
+
+@media only screen and (max-width: 700px){
+  .flex-container{
+    flex-grow: row wrap;
+    flex-direction: column;  
+  }
+}
+
+.sidebar{
+  list-style:none;
+  display: flex;
+  flex-direction: column;
+  justify-content:center;
+}
+
+</style>
+<ul class="flex-container">
+  <li class="flex-item flex_item_left">1</li>
+  <li class="flex-item flex_item_left">2</li>
+  <li class="flex-item flex_item_right">3</li>
+  <li class="flex-item flex_item_right">4</li>
+</ul>
+
+
+<ul class="main__ul">
+  <li class="inside__ul">1</li>
+  <li class="inside__ul">2</li>
+  <li class="inside__ul">3</li>
+  <li class="inside__ul">4</li>
+  <li class="inside__ul">5</li>
+</ul>
+
+
+<ul class="sidebar">
+  <li class="inside__ul">1</li>
+  <li class="inside__ul">2</li>
+</ul>
+
+</body>
+</html>
